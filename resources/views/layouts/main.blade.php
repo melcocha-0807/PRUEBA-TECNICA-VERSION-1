0fd2e2778893fa5e660bb4981ddc5d565b190a51
MARIANA TORIJANO <marianatorijanoa@gmail.com>
2025-07-22 10:38:30 -0500

    DISEÑO DEFINIDO

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,10 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Tienda Virtual')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="{{ asset('css/auxiliar.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/usuario.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/vendedor.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/COMPLET.css') }}" rel="stylesheet">
 </head>
 <body>
     {{-- Barra de navegación --}}
